68e6fa61b7c6c5299dabce2ab26058dbb79f0464
Zach <zschroe2@uoregon.edu>
2022-06-06 20:13:43 -0700

    Working plots

--- app.R
+++ app.R
@@ -11,17 +11,31 @@ library(lubridate)
 library(shiny)
 
 ########################## LOADING DATA
-load("shiny_data.RData")
-d <- willsmith_deidentified_analyzed
+#load("shiny_data.RData")
+load("shiny_data_DI.RData")
 
-fig_data <- willsmith_deidentified_analyzed %>% 
-  dplyr::group_by(round_date(tweet_date, 
-                             unit = "day")) %>% 
-  summarise(.groups = 'keep',
-            ntweet = n()) %>% 
-  as_tibble() 
+fig_data <- list(NULL)
 
-names(fig_data)[1] <- "tweet_date"
+for(i in seq_along(analyzed_DI)){
+  fig_data[[i]] <- analyzed_DI[[i]] %>% 
+    dplyr::group_by(round_date(tweet_date, 
+                               unit = "month")) %>% 
+    summarise(.groups = 'keep',
+              ntweet = n()) %>% 
+    as_tibble() 
+  names(fig_data[[i]]) <- c("tweet_date", "ntweet")
+}
+
+names(fig_data) <- names(analyzed_DI)
+
+targets <- c("BBQ Becky", 
+             "Justine Sacco", 
+             "Permit Patty", 
+             "Kanye West", 
+             "Lindsay Ellis", 
+             "R. Kelly", 
+             "Will Smith", 
+             "Doja Cat")
 
 ########################## UI
 
@@ -30,16 +44,22 @@ ui <- fluidPage(
   navbarPage(
     title = "A Deep Dive into Cancel Culture",
     tabPanel(
-      title = "Timeline of Tweets Cancelling Will Smith",
+      title = "Timeline of Cancelling Tweeets",
       sidebarLayout(
         sidebarPanel(
           width = 2,
-          radioButtons(
-            inputId = "verified",
-            label = "See Analysis of Tweets By:",
-            choices = c("Verified Users", "Unverified Users", "All Users"),
-            selected = "Verified Users",
-            inline = FALSE
+          selectInput(
+            inputId = "target",
+            label = "See Analysis of Tweets Cancelling:",
+            choices = c("BBQ Becky" = "1", 
+                        "Justine Sacco" = "2", 
+                        "Permit Patty" = "3", 
+                        "Kanye West" = "4", 
+                        "Lindsay Ellis" = "5", 
+                        "R. Kelly" = "6", 
+                        "Will Smith" = "7", 
+                        "Doja Cat" = "8"),
+            selected = "BBQ Becky"
           )
         ),
         mainPanel(
@@ -53,27 +73,33 @@ ui <- fluidPage(
       sidebarLayout(
         sidebarPanel(
           width = 2,
+          selectInput(
+            inputId = "target_cc",
+            label = "See Analysis of Tweets Cancelling:",
+            choices = c("BBQ Becky" = "1", 
+                        "Justine Sacco" = "2", 
+                        "Permit Patty" = "3", 
+                        "Kanye West" = "4", 
+                        "Lindsay Ellis" = "5", 
+                        "R. Kelly" = "6", 
+                        "Will Smith" = "7", 
+                        "Doja Cat" = "8"),
+            selected = "BBQ Becky"
+          ),
           radioButtons(
             inputId = "y_axis",
             label = "Tweet Content",
-            choices = c("followers_count", "following_count", "tweet_count"),
-            selected = "followers_count",
+            choices = c("MindOverall", "negative", "positive"),
+            selected = "MindOverall",
             inline = FALSE
           ),
           radioButtons(
             inputId = "x_axis",
             label = "Tweet Variables",
-            choices = c("like_count", "reply_count", "retweet_count"),
-            selected = "like_count",
+            choices = c("MindOverall", "negative", "positive"),
+            selected = "negative",
             inline = FALSE
-          ),
-          radioButtons(
-            inputId = "verified",
-            label = "Is Verified?",
-            choices = c("Yes", "No"),
-            selected = "No"
-          )
-        ),
+          )),
         mainPanel(
           plotOutput("distPlot_cc", height = "800px")
         )
@@ -86,57 +112,50 @@ ui <- fluidPage(
 ########################## SERVER
 server <- function(input, output, session) {
   
-  ###### THE REACTIVE FUNCTIONS ARE NOT UP AND RUNNING YET, THEY'RE TOO COMPUTATIONALLY INTENSIVE, SO I'M DIGGING INTO HOW TO DO THEM FASTER 
-  #   fig_data <- #reactive({ ws %>% 
-  #     # Filtering by input
-  #     if(stringr::str_detect(input$verified, "Verified Users")){
-  #         filter(ws$verified == TRUE)}
-  #       else if(stringr::str_detect(input$verified, "Unverified Users")){
-  #         filter(ws$verified == FALSE)}
-  #       else{} %>% 
-  #     # Grouping to get nTweets per day
-  #     fig_data <- ws %>% 
-  #         dplyr::group_by(round_date(tweet_date, 
-  #                                    unit = "day")) %>% 
-  #         summarise(.groups = 'keep',
-  #                   ntweet = n()) %>% 
-  #         as_tibble() 
-
+  outVar <- reactive({
+    temp <- fig_data[[as.numeric(input$target)]]
+  })
   
   output$distPlot_ws <- renderPlotly({
-   fig <- plot_ly(fig_data, 
-                  name = "Timeline of Tweets Cancelling Will Smith",
-                  x = ~tweet_date, 
-                  y = ~ntweet, 
-                  type = 'scatter', 
-                  mode = 'lines+markers') 
-   fig %>% layout(title = "Timeline of Tweets Cancelling Will Smith",
-                          paper_bgcolor='rgb(255,255,255)', 
-                          plot_bgcolor='rgb(229,229,229)',
-                          xaxis = list(title = "Date",
-                                      gridcolor = 'rgb(255,255,255)',
-                                      showgrid = TRUE,
-                                      showline = FALSE,
-                                      showticklabels = TRUE,
-                                      tickcolor = 'rgb(127,127,127)',
-                                      ticks = 'outside',
-                                      zeroline = FALSE),
-                         yaxis = list(title = "Number of Tweets",
-                                      gridcolor = 'rgb(255,255,255)',
-                                      showgrid = TRUE,
-                                      showline = FALSE,
-                                      showticklabels = TRUE,
-                                      tickcolor = 'rgb(127,127,127)',
-                                      ticks = 'outside',
-                                      zeroline = FALSE))
+    fig <- plot_ly(outVar(), 
+                   name = "Timeline of Tweets Cancelling Will Smith",
+                   x = ~tweet_date, 
+                   y = ~ntweet, 
+                   type = 'scatter', 
+                   mode = 'lines+markers') 
+    fig %>% layout(title = paste("Timeline of Tweets Cancelling", targets[as.numeric(input$target)]),
+                   paper_bgcolor='rgb(255,255,255)', 
+                   plot_bgcolor='rgb(229,229,229)',
+                   xaxis = list(title = "Date",
+                                gridcolor = 'rgb(255,255,255)',
+                                showgrid = TRUE,
+                                showline = FALSE,
+                                showticklabels = TRUE,
+                                tickcolor = 'rgb(127,127,127)',
+                                ticks = 'outside',
+                                zeroline = FALSE),
+                   yaxis = list(title = "Number of Tweets",
+                                gridcolor = 'rgb(255,255,255)',
+                                showgrid = TRUE,
+                                showline = FALSE,
+                                showticklabels = TRUE,
+                                tickcolor = 'rgb(127,127,127)',
+                                ticks = 'outside',
+                                zeroline = FALSE))
   })
   
   
+  outVarcc <- reactive({
+    temp <- analyzed_DI[[as.numeric(input$target_cc)]]
+  })
+  
   output$distPlot_cc <- renderPlot({
-    willsmith_deidentified_analyzed %>% 
-      ggplot(aes(x = scale(!!sym(input$x_axis)),
-                 y = scale(!!sym(input$y_axis)))) +
-         geom_point()
+    ggplot(outVarcc(),
+           aes(x = !!sym(input$x_axis),
+               y = !!sym(input$y_axis))) +
+      geom_point() +
+      geom_jitter() +
+      geom_smooth()
   })
 }
 ########################## RUN
@@ -144,28 +163,6 @@ shinyApp(ui = ui, server = server)
 
 
 #### SHINY PROFILER AND PROFILING CODE; arrow package
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
